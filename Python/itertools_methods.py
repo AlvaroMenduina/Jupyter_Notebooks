5af7711eb3970da328b2a81a934d0a4c3c26f64a
AlvaroMenduina <alvaro.menduina@gmail.com>
2019-04-30 16:07:31 +0100

    itertools tricks

--- Python/itertools_methods.py
+++ Python/itertools_methods.py
@@ -64,3 +64,50 @@ if __name__ == "__main__":
     for i in itertools.chain(odd, even):
         print(i)
 
+    # Compress: filters out elements from the first iterable
+    colors = ['red', 'blue', 'green', 'yellow', 'purple']
+    truths = [0, 1, 1, 0, 1]
+    for i in itertools.compress(colors, truths):
+        print(i)
+
+    # Dropwhile(predicate, iterable): drops elements while the predicate is true, afterwards return all
+    numbers = np.arange(1, 20)
+    for i in itertools.dropwhile(lambda x: True if x <5 else False, numbers):
+        print(i)
+
+    # Filterfalse(predicate, iterable): ilters elements from iterable returning only those for which the predicate is False
+    numbers = [-1, 2, -3, 4, -5, 6, -7]
+    for i in itertools.filterfalse(lambda x: np.abs(x) - x == 0, numbers):
+        print(i)
+
+    # Groupby(iterable, key)
+    numbers = np.arange(1, 10)
+    for key, group in itertools.groupby(numbers, lambda x: x%2):
+        print(key)
+        print(list(group))
+
+    # Permutations(iterable)
+    letters = ['a', 'b', 'c']
+    for perm in itertools.permutations(letters):
+        print(perm)
+
+    # Product(iter1, iter2): Cartesian product of iterables
+    numbers = ['0', '1']
+    for prod in itertools.product(numbers, numbers):
+        print(prod)
+
+    # tee(iterable, n times)
+    colors = ['red', 'orange', 'yellow', 'green', 'blue']
+    alpha_colors, beta_colors = itertools.tee(colors)
+
+    for a, b in zip(alpha_colors, beta_colors):
+        print(a, b)
+
+    ### Recipes for extending itertools
+    def take(n, iterable):
+        "Return first n items of the iterable as a list"
+        return list(itertools.islice(iterable, n))
+
+    def quantify(iterable, pred=bool):
+        "Count how many times the predicate is true"
+        return sum(itertools.imap(pred, iterable))
